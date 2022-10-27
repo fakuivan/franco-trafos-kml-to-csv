4bb924c637cd516e6db7f1f8b7f2f72f42618f9e
fakuivan <fakuivan@gmail.com>
2022-10-27 15:19:58 -0300

    Fix custom attribute parsing

--- parse.py
+++ parse.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3.10
-from typing import NamedTuple
+from typing import NamedTuple, TypeVar, Any
 from pykml import parser
 from pykml.factory import nsmap
 from lxml import objectify
 import typer
 import csv
 from sys import stdout
+
+T = TypeVar("T")
+def getattr_chain(obj, default: T, attr_name: str, *rest_attr: str) -> T | Any:
+    if not (hasattr(obj, attr_name)):
+        return default
+    attr = getattr(obj, attr_name)
+    if len(rest_attr) > 0:
+        getattr_chain(attr, default, *rest_attr)
+    return attr
+
 
 class Trafo(NamedTuple):
     nombre_pot: str
@@ -17,13 +27,16 @@ class Trafo(NamedTuple):
     @classmethod
     def from_xml(cls, xml: objectify.ObjectifiedElement):
         long, lat, _ = map(float, str(xml.Point.coordinates).split(","))
+        schema_data = getattr_chain(xml, None, "ExtendedData", "SchemaData")
+        trafo, monohilo = ("", "") if schema_data is None else [*schema_data.getiterator()][2:4]
         return cls(
             getattr(xml, "name", ""),
-            getattr(xml, "ID_Trafo", ""),
-            getattr(xml, "ID_Monohilo", ""),
+            trafo,
+            monohilo,
             lat,
             long
         )
+
 
 def main(kml_file: typer.FileText):
     root = parser.parse(kml_file).getroot()
