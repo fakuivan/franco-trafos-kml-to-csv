2105dce579fc2c1662950f63a1cfa5596c78d1d2
fakuivan <fakuivan@gmail.com>
2022-10-27 14:18:17 -0300

    First iteration of the script

---
+++ parse.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3.10
+from typing import NamedTuple
+from pykml import parser
+from pykml.factory import nsmap
+from lxml import objectify
+import typer
+import csv
+from sys import stdout
+
+class Trafo(NamedTuple):
+    nombre_pot: str
+    id_trafo: str
+    id_monohilo: str
+    lat: float
+    long: float
+
+    @classmethod
+    def from_xml(cls, xml: objectify.ObjectifiedElement):
+        *coords, = map(float, str(xml.Point.coordinates).split(",")[:2])
+        return cls(
+            getattr(xml, "name", ""),
+            getattr(xml, "ID_Trafo", ""),
+            getattr(xml, "ID_Monohilo", ""),
+            coords[1],
+            coords[0]
+        )
+
+def main(kml_file: typer.FileText):
+    root = parser.parse(kml_file).getroot()
+    placemarks = root.findall(".//ns:Placemark", namespaces={"ns": nsmap[None]})
+
+    writer = csv.writer(stdout)
+    writer.writerow(Trafo._fields)
+    writer.writerows(map(Trafo.from_xml, placemarks))
+
+if __name__ == "__main__":
+    typer.run(main)
